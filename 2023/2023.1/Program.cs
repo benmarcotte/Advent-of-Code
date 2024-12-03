0366b7f0b4bba6b9b2a5e9e7d3e5ea8311e55d1b
Ben Marcotte <ben.marcotte99@gmail.com>
2024-12-03 17:22:16 -0500

    2023.1 complete

--- 2023/2023.1/Program.cs
+++ 2023/2023.1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassLibrary;
+using System.Text.RegularExpressions;
+
+var sr = new StreamReader(await InputManager.GetInputAsync(new DateTime(2023, 12, 1)));
+
+int returnal = 0;
+
+var regex = new Regex(@"(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))");
+
+for(string line = sr.ReadLine(); line != null; line = sr.ReadLine())
+{
+    var matches = regex.Matches(line);
+
+    try
+    {
+        returnal += int.Parse(matches.First().Groups.Values.Last().Value) * 10;
+    }
+    catch
+    {
+        returnal += GetNumberFromString(matches.First().Groups.Values.Last().Value) * 10;
+    }
+
+    try
+    {
+        returnal += int.Parse(matches.Last().Groups.Values.Last().Value);
+    }
+    catch
+    {
+        returnal += GetNumberFromString(matches.Last().Groups.Values.Last().Value);
+    }
+}
+
+static int GetNumberFromString(string value)
+{
+    return value switch
+    {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => 0,
+    };
+}
+
+Console.WriteLine(returnal);
