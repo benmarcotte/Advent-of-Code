886df3bbf649ca53a8aff63567471c8596f71e99
Ben <ben.marcotte99@gmail.com>
2024-12-09 02:06:36 -0500

    day 7, 8 complete

--- 2024/2024.7/Program.cs
+++ 2024/2024.7/Program.cs
@@ -1,37 +1,69 @@
 ﻿using ClassLibrary;
+using System.Numerics;
+using System.Text.RegularExpressions;
 
 using var sr = new StreamReader(await InputManager.GetInput(new DateTime(2024, 12, 7)));
 {
     string line;
     long calibratedSum = 0;
+    long concatenatedSum = 0;
     while ((line = sr.ReadLine()) != null)
     {
         var split = line.Split(':');
         long total = long.Parse(split[0]);
         var arguments = split[1].Trim().Split(' ');
-        int permutations = (int)Math.Pow(2, arguments.Length - 1);
-        for (int i = 0; i < permutations; i++)
-        {
-            long check = 0;
-            string binaryRepresentation = i.ToString($"B{arguments.Length - 1}");
-            for (int j = 0; j <= binaryRepresentation.Length; j++)
-            {
-                if (j == 0)
-                    check = int.Parse(arguments[j]);
-                else
-                {
-                    if (binaryRepresentation[j-1] == '0')
-                        check += int.Parse(arguments[j]);
-                    else
-                        check *= int.Parse(arguments[j]);
-                }
-            }
-            if (check == total)
-            {
-                calibratedSum += check;
-                break;
-            }
-        }
+		int permutations = (int)Math.Pow(3, arguments.Length - 1);
+		
+		for (int i = 0; i < permutations; i++)
+		{
+			long check = 0;
+			var ternaryRepresentation = ToTernaryString(i, arguments.Length - 1);
+			List<long> concatenatedNumbers = [];
+			foreach (var number in arguments) concatenatedNumbers.Add(long.Parse(number));
+			for (int j = 0; j <= ternaryRepresentation.Length; j++)
+			{
+				if (j == 0)
+					check = long.Parse(arguments[j]);
+				else
+				{
+					if (ternaryRepresentation[j - 1] == '0')
+						check += long.Parse(arguments[j]);
+					else if (ternaryRepresentation[j - 1] == '1')
+						check *= long.Parse(arguments[j]);
+					else
+						check = long.Parse(check.ToString() + arguments[j]);
+				}
+			}
+			if (check == total)
+			{
+				if (!ternaryRepresentation.Contains('2'))
+					calibratedSum += check;
+				else 
+					concatenatedSum += check;
+				break;
+			}
+		}
     }
     Console.WriteLine($"Calibrated sum: {calibratedSum}");
+    Console.WriteLine($"Concatenated sum: {concatenatedSum}");
+    Console.WriteLine($"Total: {calibratedSum + concatenatedSum}");
+}
+
+
+static string ToTernaryString(int n, int stringLength)
+{
+	string returnal = "";
+	while (n > 0)
+	{
+		int t = n % 3;
+		returnal = t + returnal;
+		n -= t;
+		n /= 3;
+	}
+	if (returnal.Length < stringLength)
+	{
+		returnal = 0.ToString($"B{stringLength - returnal.Length}") + returnal;
+	}
+
+	return returnal;
 }
---
+++ 2024/2024.8/Program.cs
@@ -0,0 +1,66 @@
+﻿using ClassLibrary;
+
+List<List<char>> map = [];
+
+using var sr = new StreamReader(await InputManager.GetInput(new DateTime(2024, 12, 8)));
+{
+    string line;
+    while ((line = sr.ReadLine()) != null) map.Add(new List<char>(line));
+}
+
+Dictionary<char, HashSet<(int i, int j)>> antennae = [];
+HashSet<(int i, int j)> antinodes = [];
+HashSet<(int i, int j)> resonantAntinodes = [];
+
+FindAntennae(map, antennae);
+FindAntinodes(map, antennae, antinodes, resonantAntinodes);
+
+Console.WriteLine($"number of antinodes: {antinodes.Count}");
+Console.WriteLine($"number of resonant antinodes: {resonantAntinodes.Count}");
+
+static void FindAntennae(List<List<char>> map, Dictionary<char, HashSet<(int i, int j)>> antennae)
+{
+    for (int i = 0; i < map.Count; i++)
+    {
+        for (int j = 0; j < map[i].Count; j++)
+        {
+            if (map[i][j] != '.')
+            {
+                if (!antennae.ContainsKey(map[i][j])) antennae[map[i][j]] = [];
+                antennae[map[i][j]].Add((i, j));
+            }
+        }
+    }
+}
+
+static void FindAntinodes(List<List<char>> map, Dictionary<char, HashSet<(int i, int j)>> antennae, HashSet<(int i, int j)> antinodes, HashSet<(int i, int j)> resonantAntinodes)
+{
+    foreach (var c in antennae.Keys)
+    {
+        foreach (var (i1, j1) in antennae[c])
+        {
+            foreach (var (i2, j2) in antennae[c])
+            {
+                if ((i1, j1) != (i2, j2))
+                {
+                    int iStep = i2 - i1;
+                    int jStep = j2 - j1;
+                    var newAntinode = (0, 0);
+                    var inBounds = true;
+                    for (int k = 1; inBounds; k++)
+                    {
+                        newAntinode = (i2 + (k * iStep), j2 + (k * jStep));
+                        if (newAntinode.Item1 >= 0 && newAntinode.Item1 < map.Count && newAntinode.Item2 >= 0 && newAntinode.Item2 < map[0].Count)
+                        {
+                            if (k == 1) antinodes.Add(newAntinode);
+                            resonantAntinodes.Add(newAntinode);
+                        }
+                        else inBounds = false;
+                    }
+                }
+                else if (antennae[c].Count > 1) resonantAntinodes.Add((i1, j1));
+            }
+        }
+    }
+}
+
