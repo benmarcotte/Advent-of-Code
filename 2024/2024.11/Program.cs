fd54235e30248c3dafca8e977b7a6b6938ad0c29
Ben Marcotte <ben.marcotte99@gmail.com>
2024-12-11 16:03:58 -0500

    2024.11 complete

---
+++ 2024/2024.11/Program.cs
@@ -0,0 +1,54 @@
+﻿using ClassLibrary;
+using System.Diagnostics.CodeAnalysis;
+
+List<string> rocks = [];
+using var sr = new StreamReader(await InputManager.GetInput(new DateTime(2024, 12, 11)));
+{
+    string line = sr.ReadLine();
+    rocks = new List<string>(line.Split(' '));
+}
+
+Dictionary<(string, int), long> memory = [];
+
+int maxBlinks = 75;
+
+long count = 0;
+
+foreach (var rock in rocks)
+{
+    count += processRock(rock, maxBlinks, ref memory);
+}
+
+long processRock(string rock, int blinks, ref Dictionary<(string, int), long> memory)
+{
+    if (blinks == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        if (rock == "0")
+        {
+            if (memory.TryGetValue((rock, blinks), out long returnal)) return returnal;
+            returnal = processRock("1", blinks - 1, ref memory);
+            memory.Add((rock, blinks), returnal);
+            return returnal;
+        }
+        else if (rock.Length % 2 == 0)
+        {
+            if (memory.TryGetValue((rock, blinks), out long returnal)) return returnal;
+            returnal = processRock(rock[..(rock.Length / 2)], blinks - 1, ref memory) + processRock(long.Parse(rock[(rock.Length / 2)..]).ToString(), blinks - 1, ref memory);
+            memory.Add((rock, blinks), returnal);
+            return returnal;
+        }
+        else
+        {
+            if (memory.TryGetValue((rock, blinks), out long returnal)) return returnal;
+            returnal = processRock((long.Parse(rock) * 2024).ToString(), blinks - 1, ref memory);
+            memory.Add((rock, blinks), returnal);
+            return returnal;
+        }
+    }
+}
+
+Console.WriteLine($"number of rocks after {maxBlinks} blinks: {count}");
